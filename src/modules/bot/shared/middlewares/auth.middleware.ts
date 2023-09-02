1def562bf584eb3769b14124fdb5706d13d5b701
sajjad isvand <sajjadzibaee102@gmail.com>
2023-09-02 14:32:29 +0330

    use only in private

--- src/modules/bot/shared/middlewares/auth.middleware.ts
+++ src/modules/bot/shared/middlewares/auth.middleware.ts
@@ -2,6 +2,9 @@ import { Context } from '../interfaces/context.interface';
 import { PrismaClient } from '@prisma/client';
 const client = new PrismaClient();
 export async function AuthMiddleware(ctx: Context, next: any) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  if (ctx.update.channel_post) return;
   const user = await client.user.findUnique({
     where: { userId: ctx.from.id },
   });
