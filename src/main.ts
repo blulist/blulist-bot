259c2ff0384f8234bd7c8b1189a0e0ddb105b8e7
sajjad <sajjadzibaee102@gmail.com>
2023-09-11 12:06:03 +0000

    dockerize

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 (async () => {
   const app = await NestFactory.create(AppModule, {});
   await app.listen(4000);
