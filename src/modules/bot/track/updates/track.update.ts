fdb64139e342c0fc9fd472a8f2ea39dabab01b25
sajjad isvand <sajjadzibaee102@gmail.com>
2023-09-14 11:35:15 +0330

    feat: add logging

--- src/modules/bot/track/updates/track.update.ts
+++ src/modules/bot/track/updates/track.update.ts
@@ -1,12 +1,14 @@
 import { Action, Ctx, Sender, Update } from 'nestjs-telegraf';
-import { UseFilters, UseGuards } from '@nestjs/common';
+import { UseFilters, UseGuards, UseInterceptors } from '@nestjs/common';
 import { CheckPlaylistGuard } from '../../shared/guards/checkplaylist.guard';
 import { TrackService } from '../services/track.service';
 import { Context } from '../../shared/interfaces/context.interface';
 import { ExceptionsFilter } from '../../shared/filters/exceptions.filter';
+import { LoggingInterceptor } from '../../shared/interceptors/logging.interceptor';
 
 @Update()
 @UseFilters(ExceptionsFilter)
+@UseInterceptors(LoggingInterceptor)
 export class TrackUpdate {
   constructor(private trackService: TrackService) {}
   @Action(/selectTrack:(.*):(.*)/)
